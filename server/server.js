0d0a5285f7ebf02be40e8bc149b1436602cc124f
Matheus_WD_2006 <matheusditz.pf028@academico.ifsul.edu.br>
2026-09-12 19:05:06 -0300

    criação do schema de teste inicial

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3003;
 
 app.get("/", (req, res) => {
   res.send("Servidor rodando com sucesso!");
