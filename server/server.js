fb22059e3225525993bf6318d33c77124be2f9bc
Matheus_WD_2006 <matheusditz.pf028@academico.ifsul.edu.br>
2026-09-12 18:48:26 -0300

    aleluia esse prisma funcionou

---
+++ server/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Servidor rodando com sucesso!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
